7f953d62f2f7e3118eb5bacb5e07570e44a9efbd
indra7875 <indrajit6479@gmail.com>
2022-05-10 10:41:56 +0530

    Added FactorialRecursion Program

--- src/RecursionPower.java
+++ src/RecursionPower.java
@@ -4,6 +4,9 @@ public class RecursionPower {
     }
 
     public static int powerOfTwo(int n){
+        if (n < 0){
+            return -1;
+        }
         if (n == 0){
             return 1;
         }
