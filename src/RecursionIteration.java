a8f5f98b44b48a2d791903b661695d1435aea715
indra7875 <indrajit6479@gmail.com>
2022-05-10 09:53:23 +0530

    Added IterationPower Program

---
+++ src/RecursionIteration.java
@@ -0,0 +1,14 @@
+public class RecursionIteration {
+    public static void main(String[] args) {
+        System.out.println(powerOfTwo(4));
+    }
+    public static int powerOfTwo(int n){
+        int i = 0;
+        int power = 1;
+        while (i < n){
+            power = 2 * power;
+            i ++;
+        }
+        return power;
+    }
+}
